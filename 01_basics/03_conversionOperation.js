e376e3596c737d72fd9aeda4204ac8e5214874ce
rahulmahato216 <161539665+rahulmahato216@users.noreply.github.com>
2024-02-28 02:28:49 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let runs = "50abc"
+
+// console.log(typeof runs);
+// console.log(typeof (runs));
+
+let valueInNumber = Number(runs)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); //NaN
+
+// "50" => 50
+// "50jhf" => NaN
+// true => 1
+
+let bool  = 1
+
+let checkBoolConversion = Boolean(bool)
+console.log(checkBoolConversion); // gives true
+
+// 1=> true
+// ""=> false
+// "Rahul" => true

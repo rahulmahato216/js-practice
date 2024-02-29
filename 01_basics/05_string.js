ee4bb010ab189c45fd3ab0500d61ec4515e905fb
rahulmahato216 <161539665+rahulmahato216@users.noreply.github.com>
2024-02-29 02:53:45 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,20 @@
+const myName = "Rahul" //one way of declaring string
+
+//another is
+const myPlace = new String('Chandil-jh-in')
+
+// console.log(myPlace[0]); //C
+// console.log(myPlace.__proto__); //object type
+
+//prototype functions can be accessed
+// console.log(myPlace.toUpperCase());
+// console.log(myPlace.charAt(3));
+// console.log(myPlace.indexOf('n'));
+
+const string2 = "   bhagat   good"
+// console.log(string2.trim());
+
+const url = "http://abc.com/xyz%30fghj"
+// console.log(url.replace('%30','-'))
+
+//console.log()
